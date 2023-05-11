654f79f0328216dfb674edc849316a17af74faca
Prajval Baidger <prajwalbadiger@gmail.com>
2023-05-11 19:59:22 +0530

    Linear search Algorithm

---
+++ README.md
@@ -0,0 +1,23 @@
+## Big O
+1. growth is w.r.t. the input
+2. constants are dropped
+3. worst case is usually the way we measure
+
+
+## Data Stuctures
+
+### Arrays
+
+- contiguous memory space
+- fixed size
+- all operations in array take O(1), time taken for each cannot grow when input grows
+- a[n]  = address of a + offset(n) * width of type
+
+## Algorithms
+
+### Search
+
+1. Linear Search
+- go through each value in the array and check if its the required value
+- O(N), grows linearly with input
+
---
+++ algorithms/search/linear_search/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE_OF_ARR (10)
+#define TRUE (1)
+#define FLASE (0)
+
+int linear_search(int[], int, int);
+
+int main(int argc, char *argv[]) {
+  int arr[SIZE_OF_ARR] = {20, 1, 4, 78, 201, 23, 8, 10, 23, 54};
+  int num = 70;
+  printf("%d %s\n", num,
+         linear_search(arr, SIZE_OF_ARR, num) ? "found" : "not found");
+  return EXIT_SUCCESS;
+}
+
+int linear_search(int *haystack, int size_of_arr, int needle) {
+  int i;
+  for (i = 0; i < size_of_arr; i++) {
+    if (haystack[i] == needle)
+      return TRUE;
+  }
+  return FLASE;
+}
